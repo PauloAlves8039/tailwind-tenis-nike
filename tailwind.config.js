6e9f433c10256dc46cd83cb06e19d2d03caf4c76
PauloAlves8039 <pj38alves@gmail.com>
2024-11-16 12:04:43 -0300

    feat: personalizadas animações e keyframes

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,22 @@ module.exports = {
       },
       backgroundImage:{
         "tenis": "url('/src/assets/images/bg.png')"
+      },
+      keyframes: {
+        floatRight: {
+          "0%": { transform: "translatex(0px)" },
+          "50%": { transform: "translatex(8px)" },
+          "100%": { transform: "translatex(0px)" },
+        },
+        opacityPulse: {
+          "0%": { opacity: 1 },
+          "50%": { opacity: 0.2 },
+          "100%": { opacity: 1 },
+        }
+      },
+      animation: {
+        floatRight: "floatRight 2.5s infinite",
+        opacityPulse: "opacityPulse 2.5s infinite"
       }
     },
   },
